b164a401baa82f9815e1147b285b0107c9fb29e2
Alina Galochkina <bashkirovaalina@gmail.com>
2020-07-08 18:00:22 +0300

    ...

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const serveStatic = require('serve-static') 
 const path = require('path')
+const connectHistory = require('connect-history-api-fallback')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.use('/TotalStatistics', serveStatic(path.join(__dirname, '/TotalStatistics')))
+app.use(connectHistory())
 
 const port = process.env.PORT || 8080
 app.listen(port)

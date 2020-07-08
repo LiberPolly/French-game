88543a02422006d75223670046af82d685d58534
Alina Galochkina <bashkirovaalina@gmail.com>
2020-07-08 15:06:19 +0300

    reload impruvments

--- server.js
+++ server.js
@@ -10,3 +10,18 @@ const port = process.env.PORT || 8080
 app.listen(port)
 
 console.log('Listening on port: ' + port)
+
+
+
+
+var router = express.Router();
+
+// serve angular front end files from root path
+router.use('/', express.static('app', { redirect: false }));
+
+// rewrite virtual urls to angular app to enable refreshing of internal pages
+router.get('*', function (req, res, next) {
+    res.sendFile(path.resolve('app/index.html'));
+});
+
+module.exports = router;
